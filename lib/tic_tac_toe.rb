3b4bbfcf4d6c91d775a824b8af49835be22cc6d5
Anthony Graniero <ananfro@gmail.com>
2019-07-09 17:39:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,17 @@ class TicTacToe
     end
   end
   
-  
+  def turn(board)
+    puts 'Please enter 1-9:'
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
   
   def turn_count(board)
     counter = 0
