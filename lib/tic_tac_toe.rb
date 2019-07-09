46b7fe7c3187c9aa81aad47e951f319b5e926339
Anthony Graniero <ananfro@gmail.com>
2019-07-09 17:37:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,5 +41,14 @@ class TicTacToe
     end
   end
   
+  def turn_count(board)
+    counter = 0
+    board.each do |spot|
+      if spot == 'X' || spot == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
   
 end
