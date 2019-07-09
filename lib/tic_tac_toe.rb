4c4fec304e4beb2135ad9b53a28c8572b4afbfa5
Anthony Graniero <ananfro@gmail.com>
2019-07-09 17:33:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,4 +35,11 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
   
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+      return true 
+    end
+  end
+  
+  
 end
